ee9a6bc152c3d38246ab63905dbfbe35baf4966c
themiddlemanagement <skinnyxchris@gmail.com>
2025-07-21 04:13:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 // 🧱 Core Imports and Setup
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js';
-import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/controls/OrbitControls.js';
+import { OrbitControls } from './OrbitControls.js';
 
 // 🌌 Scene Initialization
 const scene = new THREE.Scene();
